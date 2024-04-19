dde7389f8b3c6747e8df8f93f9c5465563da64a1
kupferb <mjmj4@walla.com>
2024-04-19 16:09:29 +0300

    v

--- run_frfrcm_segmentation.m
+++ run_frfrcm_segmentation.m
@@ -1,13 +1,11 @@
-clearvars
+function [PRI_all, VoI_all, GCE_all, BDE_all, im_names ]= run_frfrcm_segmentation(data_set, Nseg)
 close all
-data_set = "bsd500";
 run_type = "test";
 if strcmp(getenv('computername'),'BENNYK')
     base_ssp_path = 'C:\Users\Benny\MATLAB\Projects\Segmentation-Using-Superpixels';
     if data_set=="bsd300"
         bsdsRoot = 'C:\Users\Benny\MATLAB\Projects\AF-graph\BSD';
     end
-    data_dir = "C:\Study\runs\bsd\test\4_funcs_FH_rgb_0.8_300_250_120";
 else
     base_ssp_path = 'D:\MATLAB\github\Segmentation-Using-Superpixels';
     if data_set=="bsd300"
@@ -16,7 +14,6 @@ else
         bsdsRoot = 'D:\DataSet\BSD\500\BSDS500\data';
         gt_seg_root = 'D:\DataSet\BSD\500\BSDS500\data\groundTruth';
     end
-    data_dir = "D:\Study\runs\bsd500\results\test\4_funcs_FH_rgb_0.8_300_250";
 
 end
 addpath(fullfile(base_ssp_path,'others'))
@@ -58,35 +55,30 @@ PRI_all = zeros(Nimgs,1);
 VoI_all = zeros(Nimgs,1);
 GCE_all = zeros(Nimgs,1);
 BDE_all = zeros(Nimgs,1);
+im_names = cell(Nimgs,1);
 se=3; % the parameter of structuing element used for morphological reconstruction
 w_size=3; % the size of fitlering window
 
-for k_idxI = 1:Nimgs%64:Nimgs
+parfor k_idxI = 1:Nimgs%64:Nimgs
     idxI = Nimgs_inds(k_idxI);
         img_name = int2str(BSDS_INFO(1,idxI));
     img_loc = fullfile(bsdsRoot,'images','test',[img_name,'.jpg']);    
     if ~exist(img_loc,'file')
         img_loc = fullfile(bsdsRoot,'images','train',[img_name,'.jpg']);
     end
-    
-    I = imread(img_loc);
-    Nseg = 4;
-    [~,U1,~,~]=FRFCM_c(double(I),Nseg,se,w_size);
-    NcutEigenvectors = U1';
-    evec = bsxfun( @rdivide, NcutEigenvectors, sqrt(sum(NcutEigenvectors.*NcutEigenvectors,2)) + 1e-10 );
-%     evec = NcutEigenvectors;
-    % k-means
-    labels = k_means(evec',Nseg);
-    label_img = reshape(labels,size(I(:,:,1)));
+    im_names{k_idxI} = [img_name,'.jpg'];
+    fprintf('%d %d\n',Nseg, k_idxI);
+    f_ori = double(imread(img_loc));
+    [~,U1,~,~]=FRFCM_c(double(f_ori),Nseg,se,w_size);
+    [~,label_img]=fcm_image_color(f_ori,U1);
 
-%     [f_seg,label_img]=fcm_image_color(I,U1);
-    gt_imgs = readSegs(bsdsRoot,'color',str2double(img_name));
+    gt_imgs = readSegs(bsdsRoot,'gray',str2double(img_name));
     out_vals = eval_segmentation(label_img,gt_imgs);
-    fprintf('%6s: %2d %9.6f, %9.6f, %9.6f, %9.6f\n', img_name, Nseg, out_vals.PRI, out_vals.VoI, out_vals.GCE, out_vals.BDE);
+%     fprintf('%6s: %2d %9.6f, %9.6f, %9.6f, %9.6f\n', img_name, Nseg, out_vals.PRI, out_vals.VoI, out_vals.GCE, out_vals.BDE);
     
     PRI_all(k_idxI) = out_vals.PRI;
     VoI_all(k_idxI) = out_vals.VoI;
     GCE_all(k_idxI) = out_vals.GCE;
     BDE_all(k_idxI) = out_vals.BDE;
 end
-fprintf('Mean: %14.6f, %9.6f, %9.6f, %9.6f \n', mean(PRI_all), mean(VoI_all), mean(GCE_all), mean(BDE_all));
+% fprintf('Mean: %14.6f, %9.6f, %9.6f, %9.6f \n', mean(PRI_all), mean(VoI_all), mean(GCE_all), mean(BDE_all));
